b7e325b67bd302c0d8bd9f53838b43352f328cac
fin <fj335962@truro-penwith.ac.uk>
2025-01-21 16:02:52 +0000

    added subtraction

--- maths.py
+++ maths.py
@@ -1,12 +1,18 @@
 def addition(a, b):
     print(a, " + ", b, " = ", a+b)
 
+
 def multiply(a, b):
     print(a, " X ", b, " = ", a*b)
 
-def numbers():
-    inputa = input("enter number 1: ")
-    inputb = input("enter number 2: ")
+def subtract(a, b):
+    print(a, " - ", b, " = ", a-b)
 
-print(numbers(), addition(int(inputa),int(inputb)))
-print(numbers, multiply(int(inputa),int(inputb)))
+
+
+inputa = input("enter number 1: ")
+inputb = input("enter number 2: ")
+
+print(addition(int(inputa),int(inputb)))
+print(multiply(int(inputa),int(inputb)))
+print(subtract(int(inputa),int(inputb)))
