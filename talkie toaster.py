e78b408ba311aac06016dd7ae88bf7e062995a2d
OKeast <ok344493@truro-penwith.ac.uk>
2025-01-21 15:37:52 +0000

    Added comments to the Talkie Toaster program

--- talkie toaster.py
+++ talkie toaster.py
@@ -1,7 +1,9 @@
 index = 0
+# This list contains  different baked goods that will be displayed later
 breakfast = ["toast","muffins","baguettes","teacakes","buns","baps","bagels","croissants","pancakes","waffles","flapjacks"]
 breakfastloop = 0
 
+# This is Talkie Toaster's introduction from Red Dwarf
 print("Howdy Doodily Doo!")
 print("How's it going?")
 print("I'm Talkie, Talkie Toaster")
@@ -17,6 +19,8 @@ if toast == "yes" or toast == "y":
         print("|     ||  |     ||")
         print("'-----'`  '-----'`")
 
+# If the user types no or n, the 'breakfastloop' variable will be set to 1. 
+# while the variable is set to 1 the program will loop and increase the index variable so it will print the next item from the 'breakfast' list
 if toast == "no" or toast == "n":
     breakfastloop = 1 
 while breakfastloop == 1:
@@ -25,8 +29,11 @@ while breakfastloop == 1:
     toast = input("how about it? ")
     if index == 10:
         index = 0
+    # The loop will not end unless the user says yes to an item.
     if toast == "yes" or toast == "y":
         breakfastloop = 0
+        # No matter what item you want, you're getting toast.
+        # It's a toaster, not a miracle worker...
         print("Brilliant! heres some toast!")
         print("  __  _     __  _")
         print("(     ))  (     ))")
