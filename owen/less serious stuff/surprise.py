22a4fec2d602a169f1e8b8dd25f6bfc5d22623b0
OKeast <ok344493@truro-penwith.ac.uk>
2025-01-28 10:34:02 +0000

    what is the surprise???

--- owen/less serious stuff/surprise.py
+++ owen/less serious stuff/surprise.py
@@ -3,5 +3,5 @@ thechoice = 0
 while thechoice != "yes":
     thechoice = input("Will you be brave and find out what the surprise is?")
 if thechoice == "yes":
-    webbrowser.open('https://tinyurl.com/3k937b4f') 
+    webbrowser.open('https://r.mtdv.me/shhsurprise') 
 # Little secret 
