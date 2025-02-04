47a598285265e55623683b370fdf723d444cb169
Me318209 <olivergazzard05@gmail.com>
2025-02-04 15:10:49 +0000

    Made this thing (really good commit message)

---
+++ olly/thingy.py
@@ -0,0 +1,10 @@
+#Owen did a thing like this so I am too
+
+import webbrowser
+import time
+
+print("You don't get a choice")
+time.sleep(1)
+print("Woe be upon ye")
+time.sleep(1)
+webbrowser.open('matias.me/nsfw') 
