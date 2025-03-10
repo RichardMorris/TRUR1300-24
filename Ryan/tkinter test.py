585989a16eb3314d51c6183f3eed41b555b06f58
ToastnotAI <ryanchristie03@gmail.com>
2025-03-10 10:01:00 +0000

    fixed spelling errors and bug with font

--- Ryan/tkinter test.py
+++ Ryan/tkinter test.py
@@ -1,11 +1,12 @@
 from tkinter import *
+from tkinter import font
 
 def welcome():
     name = text_box1.get(1.0, END)
     text_box2.delete(1.0, END)
     text_box2.insert(END, "Welcome " + name)
 
-root = TK()
+root = Tk()
 root.geometry("300x300")
 frame = Frame(root)
 frame.pack()
@@ -20,7 +21,8 @@ button = Button(frame, text="Welcome", command=welcome)
 button.pack()
 
 text_box2 = Text(frame, height=1, width=20)
-text_box2.congig(font("Arial", 20))
+arial_font = font.Font(family="Arial", size=20)
+text_box2.config(font=arial_font)
 text_box2.pack()
 
 root.mainloop()
