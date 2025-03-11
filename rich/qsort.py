d32e5a1f61d6181dfa6a0e81942f02b1dd31d645
OKeast <ok344493@truro-penwith.ac.uk>
2025-03-11 15:47:27 +0000

    FIXED: Found multiple bugs within qsort.py, now displays multiple of the same number if present and will not accept negative numbers into list

--- rich/qsort.py
+++ rich/qsort.py
@@ -4,9 +4,12 @@ def qsort(data):
     pivot = data[0]
     lower = [x for x in data if x < pivot]
     higher = [x for x in data if x > pivot]
+    # added middle for if there are multiple of the same number present
+    middle = [x for x in data if x == pivot]
     qlower = qsort(lower)
     qhigher = qsort(higher)
-    return qlower + [pivot] + qhigher
+    
+    return qlower + middle + qhigher
 
 def cheatsort(data):
     counts = [0] * 100
@@ -19,13 +22,26 @@ def cheatsort(data):
         
     return result
 
+# will be set to 1 if number is negative or not -1
+validation = 0
+
 if __name__ == "__main__":
     data = []
     while True:
-        print("Enter positive number -1 to end")
+        # at the start of each loop, validation is set to 0 as to not let prior numbers intefere when appending new numbers
+        validation = 0
+        print("Enter positive number, enter -1 to end")
         num = int(input())
-        if num ==-1:
+
+        # if validation == 1 the number won't be appended to the list
+        if num <-1:
+            print ("That is not a positive number, please try another number.")
+            validation = 1
+        if num == -1:
             break
-        data.append(num)
+
+        # if validation == 0 the number is valid and will be appended to the list
+        if validation == 0:
+            data.append(num)
     sorted = qsort(data)
     print(sorted)
